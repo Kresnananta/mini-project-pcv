9780d4b88b6a8d9adeef85cdb04251972a87f70d
Kresnananta <akresnaananta@gmail.com>
2026-04-02 18:28:47 +0700

    added median filter, tight plot layout

--- mini-project-1/proj1.py
+++ mini-project-1/proj1.py
@@ -4,35 +4,82 @@ import matplotlib.pyplot as plt
 
 img_noisy = cv2.imread('../img/test_image_lena_noisy.png', cv2.IMREAD_GRAYSCALE)
 
+
+
 if img_noisy is None:
     print('Image not found! please check your file')
 
-def plot_histogram_manual(image, title):
-    hist = np.zeros(256, dtype=int)
+def plot_histogram(image):
+    hist = np.zeros(256, dtype=int) # array kosong noisy
 
     tinggi, lebar, = image.shape
 
     for i in range(tinggi):
         for j in range(lebar):
             nilai_piksel = image[i, j]
-            hist[nilai_piksel] += 1
+            hist[nilai_piksel] += 1 # masukin ke array
+    
+    return hist
 
-    plt.figure(figsize=(12, 5))
+def apply_median_filter(image, kernel_size=3):
+    print('loading median filter ...')
+    hist = np.zeros(256, dtype=int) # array median filter
 
-    # gambar
-    plt.subplot(1, 2, 1)
-    plt.imshow(image, cmap='gray', vmin=0, vmax=255)
-    plt.title('Citra')
+    offset = kernel_size // 2
+    tinggi, lebar = image.shape
+    result = np.zeros((tinggi, lebar), dtype=np.uint8)
+
+    padded_image = np.pad(image, offset, mode='constant', constant_values=0)
+
+    for i in range(tinggi):
+        for j in range(lebar):
+            window = padded_image[i : i + kernel_size, j : j + kernel_size]
+
+            window_flat = window.flatten()
+            window_sorted = np.sort(window_flat)
+            median_value = window_sorted[len(window_sorted) // 2]
+
+            result[i, j] = median_value
+
+            hist[result[i, j]] += 1
+    
+    return result, hist
+
+
+
+if __name__ == '__main__':
+    # histogram image with noise 
+    hist_noisy = plot_histogram(img_noisy)
+
+    img_denoised, hist_denoised = apply_median_filter(img_noisy, kernel_size=3)
+
+    plt.figure(figsize=(12, 7))
+
+    # gambar noisy
+    plt.subplot(2, 2, 1)
+    plt.imshow(img_noisy, cmap='gray', vmin=0, vmax=255)
+    plt.title('Image noisy')
     plt.axis('off')
 
-    # histogram
-    plt.subplot(1, 2, 2)
-    plt.bar(range(256), hist, color='black', width=1)
-    plt.title(title)
+    # histogram noisy
+    plt.subplot(2, 2, 2)
+    plt.bar(range(256), hist_noisy, color='black', width=1)
+    plt.title('Histogram noisy')
     plt.xlabel('Intensitas pixel (0-255)')
     plt.ylabel('Frekuensi')
 
-    plt.show()
+    # gambar denoise
+    plt.subplot(2, 2, 3)
+    plt.imshow(img_denoised, cmap='gray', vmin=0, vmax=255)
+    plt.title("Sesudah: Median Filter")
+    plt.axis('off')
 
-if img_noisy is not None:
-    plot_histogram_manual(img_noisy, 'Histogram rusak')
+    # histogram denoise
+    plt.subplot(2, 2, 4)
+    plt.bar(range(256), hist_denoised, color='black', width=1)
+    plt.title('Histogram denoised')
+    plt.xlabel('Intensitas pixel (0-255)')
+    plt.ylabel('Frekuensi')
+
+    plt.tight_layout()
+    plt.show()
